8b733d8b8e751071795281e942058cd73be86bd5
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2023-11-29 20:38:47 -0300

    cria função  para limpar elments e conecta ao btn

--- main.js
+++ main.js
@@ -6,15 +6,21 @@ const olSaida = document.querySelector('#saida');
 const btnAdicionar = document.querySelector('#adicionar');
 const btnLimpar = document.querySelector('#limpar');
 
+btnLimpar.addEventListener('click', limparElementosDaLista);
+
 atualizarItensDeLista();
-limparLista();
-atualizarItensDeLista();
+
+function limparElementosDaLista()
+{
+    limparLista();
+    atualizarItensDeLista();
+}
 
 function atualizarItensDeLista()
 {
     olSaida.innerHTML = "";
     const lista = getLista();
-    for (let i = 0;i<lista.lenght; i++)
+    for (let i = 0;i<lista.length; i++)
     {
         const item = lista[i];
         const li = document.createElement('li');
