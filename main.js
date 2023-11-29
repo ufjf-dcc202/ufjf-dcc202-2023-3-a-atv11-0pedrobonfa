a0d4dea4367ce886190a326846ea59e2a44e0316
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2023-11-29 20:23:16 -0300

    adiciona função em main.js

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 
+import { getLista } from "./lista.js";
 //Passo 1 - Ache os elementos importantes
 const pEntrada = document.querySelector('#entrada');
 const olSaida = document.querySelector('#saida');
 const btnAdicionar = document.querySelector('#adicionar');
 const btnLimpar = document.querySelector('#limpar');
 
+atualizarItensDeLista();
+atualizarItensDeLista();
+
+function atualizarItensDeLista()
+{
+    olSaida.innerHTML = "";
+    const lista = getLista();
+    for (let i = 0;i<lista.lenght; i++)
+    {
+        const item = lista[i];
+        const li = document.createElement('li');
+        li.textContent = item;
+        olSaida.appendChild(li);
+    }
+}
